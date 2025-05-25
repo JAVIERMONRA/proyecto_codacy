f5c7cd12cff47196f11d5995b237b37ef1d1e477
JAVIERMONRA <javiermonterorz232@gmail.com>
2025-05-25 14:05:43 -0500

    Add files via upload

--- utilidades.py
+++ utilidades.py
@@ -1,19 +1,12 @@
-def sumar(a, b):
-    return a + b
-
-def restar(a, b):
-    return a - b
-
-def multiplicar(a, b):
-    return a * b
-
-def dividir(a, b):
-    if b == 0:
-        return "Error: división por cero"
-    return a / b
-
-def operacion_innecesaria(x):
-    if x > 0:
-        return True
-    else:
-        return True  # Error: condicional innecesaria
+def sumar(a, b): 
+    return a + b 
+ 
+def restar(a, b): 
+    return a - b 
+def multiplicar(a, b): 
+    return a * b 
+ 
+def dividir(a, b): 
+    if b == 0: 
+        return "Error: división por cero" 
+    return a / b 
